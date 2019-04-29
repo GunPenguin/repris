e616445eeb04338c425bffa47c39ec91587fbaca
GunPenguin <50119515+GunPenguin@users.noreply.github.com>
2019-04-30 00:26:20 +0300

    Update ColiisionDamage.cs

--- Assets/Scripts/ColiisionDamage.cs
+++ Assets/Scripts/ColiisionDamage.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ColiisionDamage : MonoBehaviour
 {
+//Kto prochital tot sdoxnet :)
     // Start is called before the first frame update
     void Start()
     {
